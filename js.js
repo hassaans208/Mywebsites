da2df46a20644d13be076c8dbf43547c52c27e1d
Hassaan Shariq <hassaans208@gmail.com>
2022-05-01 15:59:48 +0500

    m

--- js.js
+++ js.js
@@ -1,16 +1,8 @@
-// // Set up any config you need (you might not need this)
-// requirejs.config({
-//   basePath: "/scripts"
-// });
-
-// // Tell RequireJS to load your main module (and its dependencies)
-// require("mainmodule");
-
 $(function(){
-  $("button").on("click", function(){
-$("p").toggle();
-});
-$("button").on("click", function(){
-$("#tx").show(1000);
-});
+  $("#hide").click(function(){
+    $("#grid-nav").hide(1000);
+  });
+  $("#show").click(function(){
+    $("#grid-nav").show(1000);
+  });
 });
